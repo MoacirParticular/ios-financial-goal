846e114bb79dc47221bab9379f7f58b5c1a1f7df
leojportes <leojportes@gmail.com>
2021-06-15 18:02:37 -0300

    feat(EFG-34: Criação das Constants)

---
+++ financialGoal/financialGoal/Sources/Infrastructure/Constants/Login/StringConstants.swift
@@ -0,0 +1,16 @@
+//
+//  StringConstants.swift
+//  financialGoal
+//
+//  Created by Leonardo Oliveira Portes on 15/06/21.
+//
+
+import Foundation
+
+
+let placeHolderUsername:String = "  username"
+let placeHolderPassword:String = "  senha"
+
+
+let titleButtonLogin:String = "LOGIN"
+let titleButtonPassword:String = "esqueci minha senha"
