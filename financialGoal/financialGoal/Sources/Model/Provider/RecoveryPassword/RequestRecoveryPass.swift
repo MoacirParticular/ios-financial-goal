4d920cd18687780d2f9f8d9fd945824b20a1a49f
leojportes <leojportes@gmail.com>
2021-06-22 18:36:58 -0300

    feat(EFG-60): Alterando requisiçoes do recoveryPassword.

--- financialGoal/financialGoal/Sources/Model/Provider/RecoveryPassword/RequestRecoveryPass.swift
+++ financialGoal/financialGoal/Sources/Model/Provider/RecoveryPassword/RequestRecoveryPass.swift
@@ -1,8 +1,35 @@
 //
-//  RequestRecoveryPass.swift
+//  requestForgotPassword.swift
 //  financialGoal
 //
-//  Created by Leonardo Oliveira Portes on 22/06/21.
+//  Created by Leonardo Oliveira Portes on 21/06/21.
 //
 
 import Foundation
+
+typealias returnCompletionRecoveryPass = (Result<ReturnDecode, Error>) -> Void
+
+protocol RequestRecoveryPassProtocol {
+    func recoveryPass(_ username: String, completionHandler: @escaping returnCompletion)
+}
+
+final class RequestRecoveryPass: RequestRecoveryPassProtocol {
+    
+    func recoveryPass(_ username: String, completionHandler: @escaping returnCompletion) {
+        let bodyTask = RequestBuilderRecoveryPass().recoveryPass(username)
+        let session = URLSession.shared
+        let task = session.dataTask(with: bodyTask) { (data, response, error) in
+            if let error = error {
+                completionHandler(.failure(error))
+            } else if let data = data {
+                do {
+                    let dataResponse = try JSONDecoder().decode(ReturnDecode.self, from: data)
+                    completionHandler(.success(dataResponse))
+                } catch let error as NSError {
+                    completionHandler(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+}
---
+++ financialGoal/financialGoal/Sources/Model/Provider/Sign_in/ApiUrlRequest.swift
@@ -0,0 +1,8 @@
+//
+//  ApiUrlRequest.swift
+//  financialGoal
+//
+//  Created by Leonardo Oliveira Portes on 22/06/21.
+//
+
+import Foundation
