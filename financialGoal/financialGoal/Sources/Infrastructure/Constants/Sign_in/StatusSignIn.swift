ac6f33ca90ca4275423ed585efa0b265f10cf6d8
Jonattan <jonattan.sousa@gmail.com>
2021-06-24 14:32:10 -0300

    feat(EFG-72): Realocado enum StatusSignIn, criado constant let para navigation.removeLast(), ativado preenchimento do textField da Login.

---
+++ financialGoal/financialGoal/Sources/Infrastructure/Constants/Sign_in/StatusSignIn.swift
@@ -0,0 +1,13 @@
+//
+//  StatusSignIn.swift
+//  financialGoal
+//
+//  Created by Jonattan Moises Sousa on 24/06/21.
+//
+
+import Foundation
+
+enum StatusSignIn {
+    case Success
+    case Failure
+}
